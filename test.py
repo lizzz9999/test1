8be491ed9873d6ce460f44b3c11f47e6cf389fe6
zgg <445669386@qq.com>
2018-11-01 20:55:54 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,50 @@
+# string = '20-JUN-11'
+# a, b, c = string.split('-')
+# print(a, b, c)
+# cc = '-'.join('a, b, c')
+# print(cc)
+
+# aa = 'ABC'
+# a = list(aa) + list('e')
+# # a = a + list('d')
+# # b = a[:] + list('e')
+# # print(b)
+# print(aa)
+
+# x = 5
+# y = x
+# y = 6
+# print(y)
+
+# a = list('acb')
+# # b = sorted(a)
+# b = a.sort()
+# print(b)
+# print(a)
+
+# a = [1, 2, 3, ['a', 'b', 'c']]
+# b = a
+# b[0] = 'haha'
+# print(id(a), id(b))
+
+# b = a[:]
+# b[-1][2] = 'haha'
+# a[0] = 0
+# print(b)
+# print(a)
+
+# a = list('123')
+# b = a.copy()
+# b[0] = 0
+# a[-1] = 0
+# print(b, a)
+
+# import datetime
+# datetime.timedelta()
+
+
+
+# a = 1+ 2
+# print(a)
+
+a = list('ABCDEF')
